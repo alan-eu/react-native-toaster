47f53296380d662d73faee6e36e67933ad7efade
Andrii Uhryn <ugrin14081994@gmail.com>
2019-12-19 22:04:45 +0200

    Fix styles

--- src/ToastStyles.ios.js
+++ src/ToastStyles.ios.js
@@ -1,34 +1,34 @@
 import { StyleSheet } from 'react-native'
-import { getStatusBarHeight } from 'react-native-iphone-x-helper';
+import { getStatusBarHeight } from 'react-native-iphone-x-helper'
 
 export const base = {
   container: {
-    paddingTop: getStatusBarHeight(),
+    paddingTop: getStatusBarHeight(true),
     paddingRight: 15,
     paddingBottom: 15,
-    paddingLeft: 15
+    paddingLeft: 15,
   },
   text: {
     color: '#ffffff',
-    fontWeight: 'bold'
-  }
+    fontWeight: 'bold',
+  },
 }
 
 export default {
   info: StyleSheet.create({
     container: StyleSheet.flatten([base.container, { backgroundColor: '#2487DB' }]),
-    text: base.text
+    text: base.text,
   }),
   success: StyleSheet.create({
     container: StyleSheet.flatten([base.container, { backgroundColor: 'green' }]),
-    text: base.text
+    text: base.text,
   }),
   warning: StyleSheet.create({
     container: StyleSheet.flatten([base.container, { backgroundColor: '#ec971f' }]),
-    text: base.text
+    text: base.text,
   }),
   error: StyleSheet.create({
     container: StyleSheet.flatten([base.container, { backgroundColor: 'red' }]),
-    text: base.text
-  })
+    text: base.text,
+  }),
 }
